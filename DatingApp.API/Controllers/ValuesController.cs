3d02dda99d5445a32e61209f2f095268805f28f8
kartik_rana <kartik_rana@dialog.com.au>
2020-08-20 15:29:34 +1000

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext Context;
+
+        public ValuesController(DataContext context)
+        {
+            Context = context;
+        }
+
+        // GET: api/Values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await Context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET: api/Values/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await Context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST: api/Values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
